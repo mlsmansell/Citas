ede754e3beeb2b1375ea2d01f7790d0ae1df6079
MLuisa Santos <mls_mansell@outlook.com>
2021-01-26 12:51:49 +0100

    Practica react y hooks terminada

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { Fragment, useState, useLayoutEffect } from 'react';
+import Formulario from './components/Formulario';
+import Cita from './components/cita';
+import { useEffect } from 'react';
+
+function App() {
+
+  //citas en local storage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales) {
+    citasIniciales = [];
+  }
+
+  //arreglo de citas
+  const [citas, guardarCitas] = useState(citasIniciales);
+
+  //useEffect para realizar ciertas operaciones cuando el state cambia
+  useEffect(() => {
+
+    let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+
+    if (citasIniciales) {
+      localStorage.setItem('citas',JSON.stringify(citas))
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]))
+    }
+
+  }, [citas]);
+
+  //funcion que coge las citas actuales y agrega la nueva
+  const crearCita = cita => {
+    guardarCitas([
+      ...citas,
+      cita
+    ])
+  }
+//funcion que elimina una cita por su id
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  }
+
+  //mensaje condicional
+
+const titulo = citas.length === 0 ? 'no hay citas' : 'Administra tus citas'
+
+  return (
+    <Fragment>
+      <h1>Administrador de pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario
+              crearCita = {crearCita}
+            />
+          </div>
+          <div className="one-half column">
+            <h2>{titulo}</h2>
+            {citas.map(cita => (
+              <Cita 
+                key = {cita.id}
+                cita={cita}
+                eliminarCita={eliminarCita}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </Fragment>
+
+  );
+}
+
+export default App;
